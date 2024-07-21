e24e04929daa668d86c89f5f204994bf8194c893
Yongho Kim <drata313@gmail.com>
2024-07-21 13:33:58 +0900

    group routes, metadata

--- app/(home)/page.tsx
+++ app/(home)/page.tsx
@@ -1,3 +1,7 @@
+export const metadata = {
+  title: 'Home',
+};
+
 export default function RootPage() {
   return <h2>Root Page</h2>;
 }
--- app/about/page.tsx
+++ app/about/page.tsx
@@ -1,3 +1,7 @@
+export const metadata = {
+  title: 'About Us',
+};
+
 export default function About() {
-    return <h1>hey guys</h1>
+  return <h1>hey guys</h1>;
 }
