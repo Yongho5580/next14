48ff483ed55fe926d84e69aaf43d9d938204db5d
Yongho Kim <drata313@gmail.com>
2024-07-21 22:47:27 +0900

    error handling

--- app/(home)/page.tsx
+++ app/(home)/page.tsx
@@ -1,19 +1,28 @@
 import { Metadata } from 'next';
+import Link from 'next/link';
 
 export const metadata: Metadata = {
   title: 'Home',
 };
 
-const URL = 'https://nomad-movies.nomadcoders.workers.dev/movies';
+export const API_URL = 'https://nomad-movies.nomadcoders.workers.dev/movies';
 
 async function getMovies() {
-  await new Promise((resolve) => setTimeout(resolve, 5000));
-  const response = await fetch(URL);
+  await new Promise((resolve) => setTimeout(resolve, 1000));
+  const response = await fetch(API_URL);
   const json = await response.json();
   return json;
 }
 
 export default async function HomePage() {
   const movies = await getMovies();
-  return <h2>{JSON.stringify(movies)}</h2>;
+  return (
+    <h2>
+      {movies.map((movie) => (
+        <li>
+          <Link href={`/movies/${movie.id}`}>{movie.title}</Link>
+        </li>
+      ))}
+    </h2>
+  );
 }
